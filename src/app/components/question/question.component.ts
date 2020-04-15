63b3bd6e22bfc83528739b593907a2de0a9b8973
ZacznijProgramowac <zacznijprogramowac@gmail.com>
2020-04-15 17:18:52 +0200

    Question Component

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { WordType } from '../../data/models';
+import { WordsService } from '../../services/words.service';
 
 @Component({
   selector: 'app-question',
   templateUrl: './question.component.html',
-  styleUrls: ['./question.component.css']
+  styleUrls: ['./question.component.css'],
 })
 export class QuestionComponent implements OnInit {
+  word: WordType = null;
 
-  constructor() { }
+  constructor(private wordsService: WordsService) {}
 
   ngOnInit(): void {
+    this.fetchWord();
   }
 
+  addToNouns(word: WordType): void {
+    this.wordsService.addNoun(word);
+    this.fetchWord();
+  }
+
+  addToVerbs(word: WordType): void {
+    this.wordsService.addVerb(word);
+    this.fetchWord();
+  }
+
+  check(): void {
+    this.wordsService.check();
+  }
+
+  private fetchWord(): void {
+    console.log('kon');
+    this.word = this.wordsService.getWords().shift();
+  }
 }
